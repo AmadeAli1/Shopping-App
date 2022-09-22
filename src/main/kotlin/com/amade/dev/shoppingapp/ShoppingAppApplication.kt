20699ac3f35a4c929824e348f605ad9e82ed2d67
AmadeAli1 <aliamade29@gmail.com>
2022-09-22 13:26:14 +0200

    GOOGLE CREDENTIALS

--- src/main/kotlin/com/amade/dev/shoppingapp/ShoppingAppApplication.kt
+++ src/main/kotlin/com/amade/dev/shoppingapp/ShoppingAppApplication.kt
@@ -1,14 +1,26 @@
 package com.amade.dev.shoppingapp
 
-import kotlinx.coroutines.delay
-import org.springframework.boot.CommandLineRunner
+import com.google.cloud.spring.autoconfigure.core.GcpProperties
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.core.io.FileUrlResource
 import org.springframework.scheduling.annotation.EnableAsync
+import java.io.File
 
 @EnableAsync
 @SpringBootApplication
-class ShoppingAppApplication
+class ShoppingAppApplication(properties: GcpProperties) {
+    @Value("\${credentials}")
+    var config: String? = null
+
+    init {
+        println("CALLLEDDD: $config")
+        val file = File("src/main/resources/google-credentials.json")
+        file.writeText(config!!)
+        properties.credentials!!.location = FileUrlResource(file.absolutePath)
+    }
+}
 
 fun main(args: Array<String>) {
     runApplication<ShoppingAppApplication>(*args)
