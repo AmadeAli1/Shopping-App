ca4598f7b27b53058856ea54bd86ef89aa0162a7
AmadeAli1 <aliamade29@gmail.com>
2022-09-22 14:43:45 +0200

    Refactor services

--- src/main/kotlin/com/amade/dev/shoppingapp/ShoppingAppApplication.kt
+++ src/main/kotlin/com/amade/dev/shoppingapp/ShoppingAppApplication.kt
@@ -1,7 +1,6 @@
 package com.amade.dev.shoppingapp
 
 import com.google.cloud.spring.autoconfigure.core.GcpProperties
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.core.env.Environment
@@ -12,11 +11,13 @@ import java.io.File
 
 @EnableAsync
 @SpringBootApplication
-class ShoppingAppApplication(properties: GcpProperties,environment: Environment) {
+class ShoppingAppApplication(properties: GcpProperties, environment: Environment) {
     init {
         val file = File("src/main/resources/google-credentials.json")
         file.writeText(environment["credentials"]!!)
         properties.credentials!!.location = FileUrlResource(file.absolutePath)
+
+
     }
 }
 
